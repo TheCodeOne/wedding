9e8decd4a7afe41902ee84da6c168623acec7e02
Dimitrios Kokkonias <dimi.kokkonias@gmail.com>
2023-08-13 15:46:24 +0200

    🎉 Add initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef, ViewChild, ElementRef, AfterViewInit, OnDestroy } from '@angular/core'
+import { FormBuilder, FormControl, Validators } from '@angular/forms'
+import { NxDialogService, NxModalRef } from '@aposin/ng-aquila/modal'
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'wedding';
+	@ViewChild('consentTemplate') consentTemplateRef!: TemplateRef<any>
+	@ViewChild('submitTemplate') submitTemplateRef!: TemplateRef<any>
+	@ViewChild('clickTriggerIcon') _clickTriggerIcon!: ElementRef<HTMLElement>
+	@ViewChild('manualTriggerIcon') _manualTriggerIcon!: ElementRef<HTMLElement>
+	dialogRef!: NxModalRef<any>
+	naturalForm = this.fb.group({
+		who: new FormControl('', [Validators.required]),
+		city: new FormControl('', [Validators.required]),
+		childrenAmount: new FormControl('', [Validators.pattern('[0-9]*'), Validators.required]),
+		hasPlusOne: new FormControl('', [Validators.required]),
+		plusOne: new FormControl(''),
+	})
+	plusOneEligable: boolean = true
+	hasPlusOne: boolean = true
+
+	constructor(readonly dialogService: NxDialogService, private readonly fb: FormBuilder) {}
+
+	showChildrenPhrase(): boolean {
+		return !!Number(this.naturalForm.get('childrenAmount')?.value)
+	}
+
+	showPlusOnePhrase(): boolean {
+		return this.naturalForm.get('hasPlusOne')?.value === 'true'
+	}
+
+	getPlusOneName(): string {
+		return this.naturalForm.get('plusOne')!.value || ''
+	}
+
+	openConsentDialog(): void {
+		this.dialogRef = this.dialogService.open(this.consentTemplateRef, {
+			ariaLabel: 'A modal with content',
+			showCloseIcon: true,
+		})
+	}
+
+	openSubmitDialog(): void {
+		this.validate()
+		if (!this.naturalForm.valid) return
+		this.dialogRef = this.dialogService.open(this.submitTemplateRef, {
+			ariaLabel: 'The final modal of the Starter App',
+			showCloseIcon: false,
+		})
+	}
+
+	closeDialog() {
+		this.dialogRef.close()
+	}
+
+	private validate() {
+		Object.values(this.naturalForm.controls).forEach(control => {
+			control?.markAsTouched({ onlySelf: true })
+		})
+	}
 }
